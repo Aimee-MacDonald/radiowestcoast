266f23ebc926c5e8b2d9748b6a7f544f55551a60
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-06-02 09:58:27 +0200

    Animated Header

--- static/scripts/preview.js
+++ static/scripts/preview.js
@@ -8,3 +8,17 @@ setInterval(function(){
   if(slideIndex >= slides.length) slideIndex = 0;
   slides[slideIndex].style.width = "100%";
 }, 2500);
+
+var lastScroll = 0;
+var header = document.getElementById("header");
+
+document.onscroll = function(e){
+  var scrollDelta = window.scrollY - lastScroll;
+  lastScroll = window.scrollY;
+
+  if(scrollDelta > 0){
+    header.style.height = "0";
+  } else {
+    header.style.height = "5rem";
+  }
+}
