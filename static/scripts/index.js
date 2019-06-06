72ec74cda218878564a23b5fdd8c65c42e0023bb
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-06-06 11:46:54 +0200

    Menu Toggle

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,14 +1,17 @@
 var el_menu = document.getElementById("menu");
 var el_open = document.getElementById("open");
+var el_close = document.getElementById("close");
 
 function closeMenu(){
   el_menu.style.width = "0";
   el_open.style.display = "block";
+  el_close.style.display = "none";
 }
 
 function openMenu(){
   el_menu.style.width = "20rem";
   el_open.style.display = "none";
+  el_close.style.display = "block";
 }
 
 var slides = document.getElementById("slider").childNodes;
