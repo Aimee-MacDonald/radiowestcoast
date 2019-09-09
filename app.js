e1b87fe1788adaf6ccbeba9911188c47769019f8
Aimee MacDonald <aimeelmacdonald@gmail.com>
2019-09-09 11:25:20 +0200

    Admin Route

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const csurf = require("csurf");
 const Subscriber = require(path.join(__dirname, "/dbmodels/subscriber"));
 const AdminUser = require(path.join(__dirname, "/dbmodels/adminUser"));
 
+const admin = require(path.join(__dirname, "/routes/admin"));
+
 mongoose.connect(process.env.DBVARS, {useNewUrlParser: true});
 
 app.set("view engine", "pug");
@@ -33,6 +35,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(csurf());
+
+app.use("/admin", admin);
 
 app.get("/", (req, res) => {
   res.redirect("/news");
@@ -56,145 +60,6 @@ app.post("/signup", (req, res) => {
     if(err) throw(err);
   });
   res.redirect("/");
-});
-
-app.get("/admin", (req, res) => {
-  if(req.isAuthenticated()){
-    AdminUser.find({_id: req.user}, function(err, docs){
-      if(err) throw err;
-
-      if(docs.length > 0){
-        if(docs[0].access === "admin"){
-          res.status(200).render("admin");
-        } else {
-          res.redirect("/admin/user");
-        }
-      }
-    });
-  } else {
-    res.redirect("/admin/login");
-  }
-});
-
-app.get("/admin/register", (req, res) => {
-  res.status(200).render("adminRegister", {csrfToken: req.csrfToken()});
-});
-
-app.post("/admin/register", (req, res) => {
-  AdminUser.find({email: req.body.email}, function(err, docs){
-    if(err) throw err;
-
-    if(docs.length > 0){
-      // User Already Exists
-      res.status(200).redirect("/admin/login");
-    } else {
-      var adminUser = new AdminUser({
-        'email': req.body.email,
-        'password': req.body.password,
-        'access': "user",
-        'visible': 'false'
-      });
-
-      adminUser.save(err => {
-        if(err) throw err;
-        AdminUser.find({email: req.body.email}, function(err, docs){
-          if(err) throw err;
-
-          if(docs.length > 0){
-            req.login(docs[0]._id, function(err){
-              if(err) throw err;
-              res.redirect("/admin/user");
-            });
-          }
-        });
-      });
-    }
-  });
-});
-
-app.get("/admin/login", (req, res) => {
-  res.status(200).render("adminLogin", {csrfToken: req.csrfToken()});
-});
-
-app.post("/admin/login", (req, res) => {
-  AdminUser.find({email: req.body.email}, function(err, docs){
-    if(err) throw err;
-
-    if(docs.length > 0){
-      bcrypt.compare(req.body.password, docs[0].password, function(err, resp){
-        if(err) throw err;
-
-        if(resp){
-          req.login(docs[0]._id, function(err){
-            if(err) throw err;
-          });
-
-          switch(docs[0].access){
-            case "user":
-              res.redirect("/admin/user");
-              break;
-            case "admin":
-              res.redirect("/admin");
-              break;
-          }
-        } else {
-          res.redirect("/admin/login");
-        }
-      });
-    } else {
-      res.redirect("/admin/login");
-    }
-  });
-});
-
-app.get("/admin/user", (req, res) => {
-  if(req.isAuthenticated()){
-    AdminUser.find({_id: req.user}, function(err, docs){
-      if(err) throw err;
-
-      if(docs.length > 0){
-        if(docs[0].access === "user"){
-          res.status(200).render("adminUser");
-        } else {
-          res.redirect("/admin");
-        }
-      }
-    });
-  } else {
-    res.redirect("/admin/login");
-  }
-});
-
-app.get("/admin/getUsers", (req, res) => {
-  //TODO: Secure This!!
-  AdminUser.find({}, function(err, docs){
-    if(err) throw err;
-
-    var returnPackage = [];
-    for(var i = 0; i < docs.length; i++){
-      var uo = {
-        'user': docs[i].email,
-        'access': docs[i].access
-      }
-
-      returnPackage.push(uo);
-    }
-    res.send({users: returnPackage});
-  });
-});
-
-app.get("/admin/getMailinglist", (req, res) => {
-  //TODO: Secure This!!
-  Subscriber.find({}, function(err, docs){
-    if(err) throw err;
-    res.status(200).send(docs);
-  });
-});
-
-app.get("/admin/logout", (req, res) => {
-  //TODO: Secure This!!
-  req.logout();
-  res.status(200).redirect("/");
 });
 
 app.get("/getTeamProfiles", (req, res) => {
---
+++ routes/admin.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const bcrypt = require("bcryptjs");
+
+const AdminUser = require(path.join(__dirname, "../dbmodels/adminUser"));
+
+router.get("/", (req, res) => {
+  if(req.isAuthenticated()){
+    AdminUser.find({_id: req.user}, function(err, docs){
+      if(err) throw err;
+
+      if(docs.length > 0){
+        if(docs[0].access === "admin"){
+          res.status(200).render("admin");
+        } else {
+          res.redirect("/admin/news");
+        }
+      }
+    });
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.status(200).render("adminLogin", {csrfToken: req.csrfToken()});
+});
+
+router.post("/login", (req, res) => {
+  AdminUser.find({email: req.body.email}, function(err, docs){
+    if(err) throw err;
+
+    if(docs.length > 0){
+      bcrypt.compare(req.body.password, docs[0].password, function(err, resp){
+        if(err) throw err;
+
+        if(resp){
+          req.login(docs[0]._id, function(err){
+            if(err) throw err;
+          });
+
+          res.redirect("/admin/news");
+        } else {
+          res.redirect("/admin/login");
+        }
+      });
+    } else {
+      res.redirect("/admin/login");
+    }
+  });
+});
+
+router.get("/news", (req, res) => {
+  if(req.isAuthenticated()){
+    res.status(200).render("admin_news");
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+router.get("/create", (req, res) => {
+  if(req.isAuthenticated()){
+    res.status(200).render("admin_create");
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+/*
+app.get("/admin/register", (req, res) => {
+  res.status(200).render("adminRegister", {csrfToken: req.csrfToken()});
+});
+
+app.post("/admin/register", (req, res) => {
+  AdminUser.find({email: req.body.email}, function(err, docs){
+    if(err) throw err;
+
+    if(docs.length > 0){
+      // User Already Exists
+      res.status(200).redirect("/admin/login");
+    } else {
+      var adminUser = new AdminUser({
+        'email': req.body.email,
+        'password': req.body.password,
+        'access': "user",
+        'visible': 'false'
+      });
+
+      adminUser.save(err => {
+        if(err) throw err;
+        AdminUser.find({email: req.body.email}, function(err, docs){
+          if(err) throw err;
+
+          if(docs.length > 0){
+            req.login(docs[0]._id, function(err){
+              if(err) throw err;
+              res.redirect("/admin/user");
+            });
+          }
+        });
+      });
+    }
+  });
+});
+
+app.get("/admin/user", (req, res) => {
+  if(req.isAuthenticated()){
+    AdminUser.find({_id: req.user}, function(err, docs){
+      if(err) throw err;
+
+      if(docs.length > 0){
+        if(docs[0].access === "user"){
+          res.status(200).render("adminUser");
+        } else {
+          res.redirect("/admin");
+        }
+      }
+    });
+  } else {
+    res.redirect("/admin/login");
+  }
+});
+
+app.get("/admin/getUsers", (req, res) => {
+  //TODO: Secure This!!
+  AdminUser.find({}, function(err, docs){
+    if(err) throw err;
+
+    var returnPackage = [];
+    for(var i = 0; i < docs.length; i++){
+      var uo = {
+        'user': docs[i].email,
+        'access': docs[i].access
+      }
+
+      returnPackage.push(uo);
+    }
+    res.send({users: returnPackage});
+  });
+});
+
+app.get("/admin/getMailinglist", (req, res) => {
+  //TODO: Secure This!!
+  Subscriber.find({}, function(err, docs){
+    if(err) throw err;
+    res.status(200).send(docs);
+  });
+});
+
+app.get("/admin/logout", (req, res) => {
+  //TODO: Secure This!!
+  req.logout();
+  res.status(200).redirect("/");
+});
+
+*/
+
+module.exports = router;
